71ec9654be4bc9cae25eeaecaf841bec137b3930
supal <supal.iut03@gmail.com>
2019-12-28 13:37:55 +0600

    hello

---
+++ src/main/java/com/example/java8/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.java8;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Java8Application.class);
+	}
+
+}
